8b47d8424ca837846c1168e2fe09d2123a5afc14
bebelicha <isabelafgm@gmail.com>
2021-11-26 14:05:54 -0300

    first commit

---
+++ game-logic.js
@@ -0,0 +1,230 @@
+var carolFernandes = 'Em Belo Horizonte - Minas Gerais, no ano de 1987, nascia Carol Fernandes. Pedagoga, escritora e ilustradora, Carol Fernandes tem uma grande paixão pela literatura infantil e busca promover em suas escritas a leitura junto a primeira infância. <br> Quer saber um pouco mais sobre a história dessa escritora e ilustradora mineira? <br> Pegue o livro <b>CORAÇÃO DO MAR</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var carolinaMariaDeJesus = 'Essa importante escritora brasileira não teve uma vida muito fácil. Sua trajetória inicia-se em 1914, quando em uma cidadezinha no interior de Minas Gerais, chamada Sacramento, veio ao mundo Carolina Maria de Jesus. Apesar de só ter estudado até o segundo ano do ensino fundamental, Carolina, em suas escritas - que são relatos do seu cotidiano, conseguia transmitir os dias sofridos e de lutas, de uma mulher preta, moradora de favela e que tirava seu sustento e dos filhos através dos mais diversos tipos de materiais recicláveis que saía a recolher pelas ruas, dia e noite. <br> Quer saber um pouco mais sobre a história completa dessa escritora autodidata? <br> Pegue o livro <b>QUARTO DE DESPEJO: DIÁRIO DE UMA FAVELADA</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var cidinhadaSilva = 'Nascia no ano de 1967, na cidade de Belo Horizonte - Minas Gerais, Cidinha da Silva. Escritora e editora na Kuanza Produções, Cidinha da Silva traz em suas obras temas como a africanidades, orixálidades, ancestralidades e os diálogos entre tradições africanas, afro-brasileiras, afro-diaspóricas e afro-indígenas relacionados com a contemporaneidade. <br> Quer saber um pouco mais sobre a história dessa grande escritora que traz a africanidade em suas obras? <br> Pegue o livro <b>OS NOVE PENTES D`ÁFRICA</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var deyseCabraldeMoura = 'Pernambucana, Dayse Cabral de Moura é pedagoga, e dedica seu trabalho a formação de professores voltados ao processo de alfabetização, educação de jovens e adultos, literatura afro-brasileira e educação das relações raciais no Brasil. <br> Quer saber um pouco mais sobre a história dessa escritora que se dedica a pesquisar sobre as questões afro-brasileiras? <br> Pegue o livro <b> ORANYAM E A GRANDE PESCARIA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var edimilsondeAlmeidaPereira = 'Nasceu no ano de 1963, na cidade de Juiz de Fora - Minas Gerais, Edimilson de Almeida Pereira. Poeta, ensaísta, professor e pesquisador da cultura e da religiosidade afro-brasileiras, Edimilson Pereira teve sua estreia na literatura em 1985, aos 22 anos, com o volume de poemas Dormundo. O autor mineiro é, indubitavelmente, um dos mais premiados da literatura brasileira. <br> Quer saber um pouco mais sobre a história desse escritor que fez pesquisas voltadas para a análise e compreensão da diversificada produção cultural oriunda da diáspora africana no Brasil? <br> Pegue o livro <b>OS COMEDORES DE PALAVRAS</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ele e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var elaineMarcelina = 'Em 1974, em Vila Aliança - uma comunidade na zona oeste do Rio de Janeiro, nasceu Elaine Cristina Marcelina Gomes. Professora, pesquisadora e historiadora, Elaine Marcelina enfrentou muitas dificuldades em sua vida, principalmente pelo fato que ela queria estudar e sua condição de vida não era muita boa. <br> Quer saber um pouco mais sobre a história dessa grande mulher que apesar de toda dificuldade, usou isso como uma forma de crescimento e não desistiu dos seus sonhos? <br> Pegue o livro <b> AS COISAS SIMPLES DA VIDA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var elzelinaDoris = ''
+var geniGuimaraes = '';
+var jaridArraes = 'Nasceu em Juazeiro do Norte - Ceará, em 1991, Jarid Arraes. Cordelista, escritora e poeta, Jarid Arraes iniciou uma importante pesquisa sobre mulheres negras, não muito conhecidas, que marcaram a história do Brasil e trouxe essas memórias em suas obras. <br> Quer saber um pouco mais sobre a história dessa grande cordelista? <br> Pegue o livro <b> HEROÍNAS NEGRAS BRASILEIRAS EM 15 CORDÉIS </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var josiasMarinhoCasadecaba = 'Na cidade de Real Forte Príncipe da Beira - Rondônia, no ano de 1979, nascia Josias Marinho de Jesus Gomes. Seu pai faleceu quando ainda era bebê e ele cresceu aprendendo muitas coisas com os irmãos. Artista, professor e pesquisador, Josias Marinho Casadecaba, lançou seu primeiro livro infanto-juvenil em 2011, mergulhando em um repertório cultural afro-brasileiro. <br> Quer saber um pouco mais sobre a história desse grande artista e escritor? <br> Pegue o livro <b> PRÍNCIPE DA BEIRA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ele e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var julioEmilioBraz = 'Nascia em 1959, no interior de Minas Gerais, na cidade de Manhumirim, Júlio Emílio Braz. Filho de uma família pobre, aos seis anos, adquiriu o hábito de leitura e começou a desenvolver seu interesse pela escrita, devido a uma tia, que trabalhava como empregada doméstica e sempre reunia os livros e revistas que as crianças da casa não queriam mais e levava para ele. <br> Quer saber um pouco mais sobre a história desse escritor da literatura infanto-juvenil? <br> Pegue o livro <b> PRETINHA, EU? </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ele e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var kiusamdeOliveira = 'Nascia em Santo André - São Paulo, Kiusam Regina de Oliveira. Artista multimídia, arte-educadora, bailarina, coreógrafa e contadora de histórias, Kiusam Oliveira, é especialista na representação do universo africano e afro-brasileiro de maneira lúdica, cativando prender a atenção das crianças de todas as cores e idades, numa postura de respeito e valorização da alteridade – atitudes mais do que necessárias nesse começo de século marcado pela intolerância para com as diferenças étnicas e sociais. <br> Quer saber um pouco mais sobre a história dessa grande escritora que traz em suas obras um olhar sobre a construção de identidades infantis e juvenis saudáveis, procurando contribuir para uma educação antirracista e para o empoderamento feminino? <br> Pegue o livro <b> OMO-OBA HISTÓRIA DE PRINCESAS </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var maduCosta = 'Em 1953, na capital do estado de Minas Gerais - Belo Horizonte, nasceu Maria do Carmo Ferreira da Costa. Graduada em Pedagogia e tendo Arte Educação como pós-graduação, Madu Costa, assim como gosta de ser chamada, é uma grande incentivadora da produção de escritas pela arte imaginativa, além de tratar da afro-brasilidade e sua expansão no campo artístico. <br> Quer saber um pouco mais sobre a história dessa escritora que dedica seus trabalhos ao público infantil e na difusão da identidade racial? <br> Pegue o livro <b> MENINAS NEGRAS </b> na estante de literatura africana e afro-brasileira do Carro- Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var neiLopes = 'Em 1942, nascia no subúrbio do Rio de Janeiro - Irajá, Nei Braz Lopes. Cantor, compositor, escritor, poeta e pesquisador, Nei Lopes é bastante conhecido pela sua dedicação à pesquisa sobre a história e a cultura afro-brasileira. Considerado uma autoridade sobre tal, ele possui mais de 35 livros publicados, entre romances, contos, dicionários e enciclopédias. <br> Quer saber um pouco mais sobre a história desse escritor que se tornou um dos grandes conhecedores da causa negra? <br> Pegue o livro <b> HISTÓRIAS DO TIO JIMBO </b> na estante de literatura africana e afro-brasileira do Carro- Biblioteca, leia para nós a bibliografia constante sobre ele e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var nilmaLinoGomes = 'Em Belo Horizonte - Minas Gerais, no ano de 1961, nascia Nilma Lino Gomes. Pedagoga, doutora em Antropologia Social e pesquisadora, Nilma Lino, marcou a história do Brasil, sendo a primeira mulher negra a comandar uma universidade pública federal, no ano de 2013. <br> Quer saber um pouco mais sobre a história dessa grande mulher que luta pela liberdade social e racial? <br> Pegue o livro <b> BETINA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca, leia para nós a bibliografia constante sobre ela e se alguém já quiser locá-lo para leitura em casa, basta levantar a mão.';
+var olegarioAlfredodaSilva = '';
+
+const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let matchCounter=0;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.autor === secondCard.dataset.autor;
+    if (isMatch){
+      matchCounter+=1;
+	    disableCards();
+    if(matchCounter==(cards.length/2)){
+      swal({title: "Você ganhou!", text: "Obrigada por jogar <", type: "success"}).then(function(){ 
+      location.reload();
+   });
+     }}
+  else{ unflipCards();
+	    console.log(firstCard.dataset.autor);
+	    console.log(secondCard.dataset.autor);
+      }
+}
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  if(firstCard.dataset.autor == "carolFernandes"){
+    Swal.fire({
+      title: "Carol Fernandes",
+      html: '<p style="text-align: left;">' + carolFernandes + '</p>' ,
+      imageUrl: 'game-images/Livros/CarolFernandesCoraçãodoMar.jpg',
+      confirmButtonColor: "#DD6B55",
+      
+    });
+} else if(firstCard.dataset.autor == "carolinaMariaDeJesus"){
+  Swal.fire({
+    title: "Carolina Maria de Jesus",
+    html: '<p style="text-align: left;">' + carolinaMariaDeJesus + '</p>',
+    imageUrl: 'game-images/Livros/CarolinaMariadeJesusQuartodeDespejo.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+ else if(firstCard.dataset.autor == "cidinhadaSilva"){
+  Swal.fire({
+    title: "Cidinha da Silva",
+    html: '<p style="text-align: left;">' + cidinhadaSilva + '</p>',
+    imageUrl: 'game-images/Livros/CidinhadaSilvaOsNovePentesDAfrica.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "deyseCabraldeMoura"){
+  Swal.fire({
+    title: "Deyse Cabral de Moura",
+    html: '<p style="text-align: left;">' + deyseCabraldeMoura + '</p>',
+    imageUrl: 'game-images/Livros/DeyseCabraldeMouraOranyameaGrandePescaria.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "edimilsondeAlmeidaPereira"){
+  Swal.fire({
+    title: "Edimilson de Almeida Pereira",
+    html: '<p style="text-align: left;">' + edimilsondeAlmeidaPereira + '</p>',
+    imageUrl: 'game-images/Livros/EdimilsondeAlmeidaPereiraOsComedoresdePalavras.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "elaineMarcelina"){
+  Swal.fire({
+    title: "Elaine Marcelina",
+    html: '<p style="text-align: left;">' + elaineMarcelina + '</p>',
+    imageUrl: 'game-images/Livros/ElaineMarcelinaAsCoisasSimplesdaVida.png',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "elzelinaDoris"){
+  Swal.fire({
+    title: "Elzelina Doris",
+    html: '<p style="text-align: left;">' + elzelinaDoris + '</p>',
+    imageUrl: 'game-images/Livros/ElzelinaDorisMulherPalavra.jpeg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "geniGuimaraes"){
+  Swal.fire({
+    title: "Geni Guimarães",
+    html: '<p style="text-align: left;">' + geniGuimaraes + '</p>',
+    imageUrl: 'game-images/Livros/GeniGuimaraesACordaTernura.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "jaridArraes"){
+  Swal.fire({
+    title: "Jarid Arraes",
+    html: '<p style="text-align: left;">' + jaridArraes + '</p>',
+    imageUrl: 'game-images/Livros/JaridArraesHeroínasNegrasBrasileiras.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "josiasMarinhoCasadecaba"){
+  Swal.fire({
+    title: "Josias Marinho Casadecaba",
+    html: '<p style="text-align: left;">' + josiasMarinhoCasadecaba + '</p>',
+    imageUrl: 'game-images/Livros/JosiasMarinhoCasadecabaPríncipedaBeira.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "julioEmilioBraz"){
+  Swal.fire({
+    title: "Júlio Emílio Braz",
+    html: '<p style="text-align: left;">' + julioEmilioBraz + '</p>',
+    imageUrl: 'game-images/Livros/JulioEmilioBrazPretinhaEu.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "kiusamdeOliveira"){
+  Swal.fire({
+    title: "Kiusam de Oliveira",
+    html: '<p style="text-align: left;">' + kiusamdeOliveira + '</p>',
+    imageUrl: 'game-images/Livros/KiusamdeOliveiraOmo-obaHistóriasdePrincesas.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "maduCosta"){
+  Swal.fire({
+    title: "Madu Costa",
+    html: '<p style="text-align: left;">' + maduCosta+ '</p>',
+    imageUrl: 'game-images/Livros/MaduCostaMeninasNegras.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "neiLopes"){
+  Swal.fire({
+    title: "Nei Lopes",
+    html: '<p style="text-align: left;">' + neiLopes + '</p>',
+    imageUrl: 'game-images/Livros/NeiLopesHistóriasdoTioJimbo.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "nilmaLinoGomes"){
+  Swal.fire({
+    title: "Nilma Lino Gomes",
+    html: '<p style="text-align: left;">' + nilmaLinoGomes + '</p>',
+    imageUrl: 'game-images/Livros/NilmaLinoGomesBetina.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "olegarioAlfredodaSilva"){
+  Swal.fire({
+    title: "Olegário Alfredo da Silva",
+    html: '<p style="text-align: left;">' + olegarioAlfredodaSilva + '</p>',
+    imageUrl: 'game-images/Livros/OlegarioAlfredodaSilvaOPentePenteia.jpg',
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+  resetBoard();
+  
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+  
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
