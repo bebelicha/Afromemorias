094f55b1731b40cbd8ab35dedd65dcd0cb5b942b
bebelicha <isabelafgm@gmail.com>
2021-11-30 04:48:30 -0300

    V1.1.0

---
+++ fase3.js
@@ -0,0 +1,177 @@
+var andrezza = '2z, poeta, escritora, produtora cultural, afroempresária, artesã e Educadora informacional. Formada em Biblioteconomia pela UFMG. Oferece experiências fluidas através da escrita.';
+var carolFernandes = 'Em Belo Horizonte - Minas Gerais, no ano de 1987, nascia Carol Fernandes. Pedagoga, escritora e ilustradora, Carol Fernandes tem uma grande paixão pela literatura infantil e busca promover em suas escritas a leitura junto a primeira infância. <br> Quer saber um pouco mais sobre a história dessa escritora e ilustradora mineira? <br> Pegue o livro <b>CORAÇÃO DO MAR</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var carolinaMariaDeJesus = 'Essa importante escritora brasileira não teve uma vida muito fácil. Sua trajetória inicia-se em 1914, quando em uma cidadezinha no interior de Minas Gerais, chamada Sacramento, veio ao mundo Carolina Maria de Jesus. Apesar de só ter estudado até o segundo ano do ensino fundamental, Carolina, em suas escritas - que são relatos do seu cotidiano, conseguia transmitir os dias sofridos e de lutas, de uma mulher preta, moradora de favela e que tirava seu sustento e dos filhos através dos mais diversos tipos de materiais recicláveis que saía a recolher pelas ruas, dia e noite. <br> Quer saber um pouco mais sobre a história completa dessa escritora autodidata? <br> Pegue o livro <b>QUARTO DE DESPEJO: DIÁRIO DE UMA FAVELADA</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var cidinhadaSilva = 'Nascia no ano de 1967, na cidade de Belo Horizonte - Minas Gerais, Cidinha da Silva. Escritora e editora na Kuanza Produções, Cidinha da Silva traz em suas obras temas como a africanidades, orixálidades, ancestralidades e os diálogos entre tradições africanas, afro-brasileiras, afro-diaspóricas e afro-indígenas relacionados com a contemporaneidade. <br> Quer saber um pouco mais sobre a história dessa grande escritora que traz a africanidade em suas obras? <br> Pegue o livro <b>OS NOVE PENTES D`ÁFRICA</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var deyseCabraldeMoura = 'Pernambucana, Dayse Cabral de Moura é pedagoga, e dedica seu trabalho a formação de professores voltados ao processo de alfabetização, educação de jovens e adultos, literatura afro-brasileira e educação das relações raciais no Brasil. <br> Quer saber um pouco mais sobre a história dessa escritora que se dedica a pesquisar sobre as questões afro-brasileiras? <br> Pegue o livro <b> ORANYAM E A GRANDE PESCARIA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var edimilsondeAlmeidaPereira = 'Nasceu no ano de 1963, na cidade de Juiz de Fora - Minas Gerais, Edimilson de Almeida Pereira. Poeta, ensaísta, professor e pesquisador da cultura e da religiosidade afro-brasileiras, Edimilson Pereira teve sua estreia na literatura em 1985, aos 22 anos, com o volume de poemas Dormundo. O autor mineiro é, indubitavelmente, um dos mais premiados da literatura brasileira. <br> Quer saber um pouco mais sobre a história desse escritor que fez pesquisas voltadas para a análise e compreensão da diversificada produção cultural oriunda da diáspora africana no Brasil? <br> Pegue o livro <b>OS COMEDORES DE PALAVRAS</b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var elaineMarcelina = 'Em 1974, em Vila Aliança - uma comunidade na zona oeste do Rio de Janeiro, nasceu Elaine Cristina Marcelina Gomes. Professora, pesquisadora e historiadora, Elaine Marcelina enfrentou muitas dificuldades em sua vida, principalmente pelo fato que ela queria estudar e sua condição de vida não era muita boa. <br> Quer saber um pouco mais sobre a história dessa grande mulher que apesar de toda dificuldade, usou isso como uma forma de crescimento e não desistiu dos seus sonhos? <br> Pegue o livro <b> AS COISAS SIMPLES DA VIDA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var elzelinaDoris = 'Elzelina Dóris dos Santos, mineira, nascida em Belo Horizonte, com formação acadêmica em Ciências Contábeis, licenciada na área de educação além de pesquisadora atua como intérprete desde 1997. DORIS é idealizadora e coordenadora do Projeto Cantando a História do Samba, que tem a finalidade de valorizar a história social do samba a partir do resgate da nossa memória musical. Tem o objetivo de despertar e desenvolver a integração social, o bem estar e a construção de uma cultura de paz e fortalecimento da identidade de todo povo brasileiro. Desenvolvido em escolas da rede pública e particular, busca o envolvimento dos professores das diversas áreas do conhecimento com oficinas de sensibilização e interativas com os alunos, possibilitando às crianças e aos jovens a fruição dessa genuína manifestação da cultura negra brasileira. O projeto articula no mesmo espaço, tempo e lugar, formação, informação, conhecimento, história e a importância da cultura afrobrasileira e de matriz africana. Com atividades lúdicas e pedagógicas com base na musicalidade rítmica do samba e na poesia, o projeto é um espaço para que aprendamos a valorizar os momentos importantes em que a música se inscreve no tempo e na historia.';
+var geniGuimaraes = 'No interior de São Paulo - no município de São Manuel, em 1947, nascia Geni Mariano Guimarães. Professora, poeta e ficcionista, Geni Guimarães, que viveu no meio rural durante sua infância, conseguiu abstrair e relatar questões socioculturais que presenciava para seus livros. Suas obras, reforçam a identidade étnica e de gênero. Para a autora, escrever é um exercício emancipatório, ou seja, ela se vale do texto também como uma forma de libertar os seus ideais para a coletividade, para não deixar que a voz do afro-descendente seja silenciada. <br> Quer saber a história completa dessa escritora? Pegue o livro <b> A COR DA TERNURA </b> na estante de literatura africana e afro-brasileira do Carro- Biblioteca.';
+var jaridArraes = 'Nasceu em Juazeiro do Norte - Ceará, em 1991, Jarid Arraes. Cordelista, escritora e poeta, Jarid Arraes iniciou uma importante pesquisa sobre mulheres negras, não muito conhecidas, que marcaram a história do Brasil e trouxe essas memórias em suas obras. <br> Quer saber um pouco mais sobre a história dessa grande cordelista? <br> Pegue o livro <b> HEROÍNAS NEGRAS BRASILEIRAS EM 15 CORDÉIS </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var josiasMarinhoCasadecaba = 'Na cidade de Real Forte Príncipe da Beira - Rondônia, no ano de 1979, nascia Josias Marinho de Jesus Gomes. Seu pai faleceu quando ainda era bebê e ele cresceu aprendendo muitas coisas com os irmãos. Artista, professor e pesquisador, Josias Marinho Casadecaba, lançou seu primeiro livro infanto-juvenil em 2011, mergulhando em um repertório cultural afro-brasileiro. <br> Quer saber um pouco mais sobre a história desse grande artista e escritor? <br> Pegue o livro <b> PRÍNCIPE DA BEIRA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var julianaTolentino = 'Poeta, professora e pesquisadora. Especialista em educação pela UFJF. Licenciada e bacharela em Ciências Sociais pela UFMG. Realiza saraus, rodas de conversas e oficinas sobre questões raciais, classe, gênero, sexualidade e literatura negra. Sua palavra, escrita e falada, navegam pelos caminhos do autoconhecimento, da ancestralidade, dos afetos e das questões sociais.';
+var julioEmilioBraz = 'Nascia em 1959, no interior de Minas Gerais, na cidade de Manhumirim, Júlio Emílio Braz. Filho de uma família pobre, aos seis anos, adquiriu o hábito de leitura e começou a desenvolver seu interesse pela escrita, devido a uma tia, que trabalhava como empregada doméstica e sempre reunia os livros e revistas que as crianças da casa não queriam mais e levava para ele. <br> Quer saber um pouco mais sobre a história desse escritor da literatura infanto-juvenil? <br> Pegue o livro <b> PRETINHA, EU? </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var kiusamdeOliveira = 'Nascia em Santo André - São Paulo, Kiusam Regina de Oliveira. Artista multimídia, arte-educadora, bailarina, coreógrafa e contadora de histórias, Kiusam Oliveira, é especialista na representação do universo africano e afro-brasileiro de maneira lúdica, cativando prender a atenção das crianças de todas as cores e idades, numa postura de respeito e valorização da alteridade – atitudes mais do que necessárias nesse começo de século marcado pela intolerância para com as diferenças étnicas e sociais. <br> Quer saber um pouco mais sobre a história dessa grande escritora que traz em suas obras um olhar sobre a construção de identidades infantis e juvenis saudáveis, procurando contribuir para uma educação antirracista e para o empoderamento feminino? <br> Pegue o livro <b> OMO-OBA HISTÓRIA DE PRINCESAS </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var lara = 'Arqueóloga, escritora e multiartista setelagoana, mestra e doutoranda em Antropologia/Arqueologia pela UFMG. Em 2021 lançou seu primeiro livro solo, "Nuvilineas" pela Alecrim Edições. Encara a arte, a escrita e a arqueopoesia como formas políticas de cura ancestral. Instagram @arqueopoesia Email: arqueopoesia@gmail.com';
+var laura = 'Mulher preta, bissexual, de axé, as vezes escreve, as vezes advoga. É bacharel e mestra em Direito pela Puc Minas. Atualmente é pesquisadora no Centro de Direitos Humanos e Empresas (CeDHE), da Fundação Getúlio Vargas.';
+var maduCosta = 'Em 1953, na capital do estado de Minas Gerais - Belo Horizonte, nasceu Maria do Carmo Ferreira da Costa. Graduada em Pedagogia e tendo Arte Educação como pós-graduação, Madu Costa, assim como gosta de ser chamada, é uma grande incentivadora da produção de escritas pela arte imaginativa, além de tratar da afro-brasilidade e sua expansão no campo artístico. <br> Quer saber um pouco mais sobre a história dessa escritora que dedica seus trabalhos ao público infantil e na difusão da identidade racial? <br> Pegue o livro <b> MENINAS NEGRAS </b> na estante de literatura africana e afro-brasileira do Carro- Biblioteca.';
+var mafuaneOliveira = 'Mafuane Oliveira é narradora de histórias, educadora, escritora e criadora do Chaveiroeiro, projeto de narração de histórias e formação de professores, voltada a pesquisa, difusão e transcriação de narrativas tradicionais africanas e afro-luso-ameríndias. A convite das embaixadas do Brasil em Moçambique e em São Tomé e Príncipe ministrou em ambos os países cursos sobre mediação de leitura e narração de histórias para professores e jovens artistas. Trabalhou como arte/educadora no Núcleo de Educação Étnico-racial da Secretaria Municipal de Educação de São Paulo e como apresentadora e roteirista do programa “Deixa que eu conto”do UNICEF-Brasil destinado a infância e a educadores durante a pandemia. Atualmente é professora de cultura brasileira, assessora de projetos culturais e tem focado suas pesquisas em ensino de cultura brasileira em contexto de educação bilíngue, literatura infanto-juvenil, diversidade cultural e oralituras quilombolas. É co-autora da publicação infanto-juvenil “Mesma Nova História”.';
+var magnaOliveira = 'MAGNA OLIVEIRA é graduada em Comunicação Social pela Uni-BH, especializada em Gestão pública, atual mestranda da FAE/UFMG, servidora da UFMG, contadora de histórias, coordenadora do projeto de extensão (Proex/UFMG), intitulado “Iranti – Ser África”, membra do Coletivo Iabas, tres mulheres pretas que contam histórias das orixás femininas. Tem poemas e artigos publicados e um livro infanto-juvenil a publicar. Leciona desde 2019 na Faculdade de Educação/FAE/UFMG, a disciplina transversal "UNI-081 - Oficina sobre aspectos da cultura africana e afro-brasileira: Nas pegadas da oralidade africana e afro-brasileira”.';
+var neiLopes = 'Em 1942, nascia no subúrbio do Rio de Janeiro - Irajá, Nei Braz Lopes. Cantor, compositor, escritor, poeta e pesquisador, Nei Lopes é bastante conhecido pela sua dedicação à pesquisa sobre a história e a cultura afro-brasileira. Considerado uma autoridade sobre tal, ele possui mais de 35 livros publicados, entre romances, contos, dicionários e enciclopédias. <br> Quer saber um pouco mais sobre a história desse escritor que se tornou um dos grandes conhecedores da causa negra? <br> Pegue o livro <b> HISTÓRIAS DO TIO JIMBO </b> na estante de literatura africana e afro-brasileira do Carro- Biblioteca.';
+var nilmaLinoGomes = 'Em Belo Horizonte - Minas Gerais, no ano de 1961, nascia Nilma Lino Gomes. Pedagoga, doutora em Antropologia Social e pesquisadora, Nilma Lino, marcou a história do Brasil, sendo a primeira mulher negra a comandar uma universidade pública federal, no ano de 2013. <br> Quer saber um pouco mais sobre a história dessa grande mulher que luta pela liberdade social e racial? <br> Pegue o livro <b> BETINA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var olegarioAlfredodaSilva = 'Natural de Teófilo Otoni - Minas Gerais, Olegário Alfredo da Silva é escritor, poeta, mestre de capoeira, contador de história, entre outras profissões. Dono de várias edições de obras sobre literatura infantil, poesias, haikai (poesia japonesa) e literatura de cordel, Olegário Alfredo se destaca também como Membro da Academia Brasileira de Cordel e Membro da Academia de Letras de Teófilo Otoni. <br> Quer saber um pouco mais sobre a história desse grande cordelista mineiro? Pegue o livro <b> O PENTE PENTEIA </b> na estante de literatura africana e afro-brasileira do Carro-Biblioteca.';
+var thomasTavares = 'Thomás Umbelino Stephan Tavares tem nove anos, nasceu em Juiz de Fora, na Zona da Mata Mineira, onde vive até hoje. Ele aproveitou o tempo de isolamento social para se dedicar à leitura e à escrita. Filho único de um casal de jornalistas, Thomás lançou em outubro de 2020 seu primeiro livro: “O Invisível que Parou o Mundo”. Thomás também faz parte de um grupo de contadores de histórias e estuda flauta doce e piano no conservatório estadual de música.';
+var miriaGomes = 'Míria Gomes de Oliveira é escritora e professora da FAE-UFMG onde integra a equipe de pesquisadores do Núcleo de Estudos Etno-Raciais e Ações Afirmativas e o Núcleo de Educação de Jovens e Adultos. Ela desenvolve pesquisas em: politicas afirmativas, formação inicial e continuada de professores e relações etnico-raciais, diversidade e interseccionalidade, educação de adultos, interação em sala de aula, ensino/aprendizagem de línguas, ensino da literatura, letramentos. Livros de poesia publicados: Máscaras de Amor e Liberdade(2015); Más Intenções(2017). Livro de contos: Carmencita(2018). Infantil:Os Batuqueiros (2020).';
+const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let matchCounter=0;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.autor === secondCard.dataset.autor;
+    if (isMatch){
+      matchCounter+=1;
+	    disableCards();
+    if(matchCounter==(cards.length/2)){
+      swal({
+        title: "Você venceu!",
+        text: "Obrigada por jogar",
+        type: "success"
+    });
+     }}
+  else{ unflipCards();
+	    console.log(firstCard.dataset.autor);
+	    console.log(secondCard.dataset.autor);
+      }
+}
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  
+if(firstCard.dataset.autor == "maduCosta"){
+  Swal.fire({
+    title: "Madu Costa",
+    html: '<p style="text-align: left;">' + maduCosta+ '</p>',
+    imageUrl: 'game-images/Livros/MaduCostaMeninasNegras.jpg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "mafuaneOliveira"){
+  Swal.fire({
+    title: "Mafuane Oliveira",
+    html: '<p style="text-align: left;">' + mafuaneOliveira + '</p>',
+    imageUrl: 'game-images/Livros/MafuaneOliveira.jpg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "magnaOliveira"){
+  Swal.fire({
+    title: "Magna Oliveira",
+    html: '<p style="text-align: left;">' + magnaOliveira + '</p>',
+    imageUrl: 'game-images/Livros/PoetasNegrasBrasileiras.jpg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "neiLopes"){
+  Swal.fire({
+    title: "Nei Lopes",
+    html: '<p style="text-align: left;">' + neiLopes + '</p>',
+    imageUrl: 'game-images/Livros/NeiLopesHistóriasdoTioJimbo.jpg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "nilmaLinoGomes"){
+  Swal.fire({
+    title: "Nilma Lino Gomes",
+    html: '<p style="text-align: left;">' + nilmaLinoGomes + '</p>',
+    imageUrl: 'game-images/Livros/NilmaLinoGomesBetina.jpg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+else if(firstCard.dataset.autor == "olegarioAlfredodaSilva"){
+  Swal.fire({
+    title: "Olegário Alfredo da Silva",
+    html: '<p style="text-align: left;">' + olegarioAlfredodaSilva + '</p>',
+    imageUrl: 'game-images/Livros/OlegarioAlfredodaSilvaOPentePenteia.jpg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+} 
+else if(firstCard.dataset.autor == "thomasTavares"){
+  Swal.fire({
+    title: "Thomás Tavares",
+    html: '<p style="text-align: left;">' + thomasTavares + '</p>',
+    imageUrl: 'game-images/Livros/ThomasTavares.jpeg',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+} 
+else if(firstCard.dataset.autor == "miriaGomes"){
+  Swal.fire({
+    title: "Miria Gomes",
+    html: '<p style="text-align: left;">' + miriaGomes + '</p>',
+    imageUrl: 'game-images/Livros/MiriaGomes.PNG',
+    imageHeight: 250,
+    confirmButtonColor: "#DD6B55",
+   
+  });
+}
+  resetBoard();
+  
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+  
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
